59a17fc2639e630c2040b6be283712fbcfdce204
Roopesh Chander <roop@roopc.net>
2021-07-13 23:29:35 +0530

    macOS: Rename MenuCommandRespondingVC to MenuCommandResponding

--- EduVPN/AppDelegate.swift
+++ EduVPN/AppDelegate.swift
@@ -268,8 +268,8 @@ extension AppDelegate {
 }
 
 extension AppDelegate {
-    private var topVC: MenuCommandRespondingViewController? {
-        environment?.navigationController?.topViewController as? MenuCommandRespondingViewController
+    private var topVC: MenuCommandResponding? {
+        environment?.navigationController?.topViewController as? MenuCommandResponding
     }
 
     @IBAction func goNextServer(_ sender: Any?) {
