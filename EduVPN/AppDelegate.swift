0fe89dc2dad951db59426798cc14c2a6abb40cbc
Roopesh Chander <roop@roopc.net>
2022-01-05 07:55:35 +0530

    dummy7

--- EduVPN/AppDelegate.swift
+++ EduVPN/AppDelegate.swift
@@ -53,6 +53,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         let window = NSApp.windows[0]
         if let navigationController = window.rootViewController as? NavigationController {
             environment = Environment(navigationController: navigationController)
+            // Dummy 7
             navigationController.environment = environment
             if let mainController = navigationController.children.first as? MainViewController {
                 mainController.environment = environment
